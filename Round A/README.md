5b47bb464d5ca7623a7661e4779d971d02302b98
DawdlingHalo <kartikgupta0100@gmail.com>
2021-05-05 00:25:42 +0530

    Code optimised and cleaned

--- Round A/README.md
+++ Round A/README.md
@@ -1,4 +1,3 @@
 # K-Goodness String
 
-The problem statement from Google Kickstart 2020 is provided [here](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3#problem) !
-The code works for the test cases. However it provides runtime error even when this works easily in colab and gives the right result.
+The problem statement from Google Kickstart 2020 is provided [here](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3#problem) ! 
--- Round A/k_goodness_string.py
+++ Round A/k_goodness_string.py
@@ -1,13 +1,8 @@
-num_case = int(input("Number of test cases:"))
-
-#empty list
-l = [] 
+num_case = int(input()) # number of test cases
 
 for i in range(num_case):
-  # adding required inputs for each test case
-  s_len = int(input("Enter string length:"))
-  exp_k = int(input("Enter expected k:"))
-  s = input("Enter string:")
+  s_len ,exp_k = map(int,input().split()) # length of string and K goodness score expected
+  s = input() # string
   
   obs_k = 0 # initalized observed k as 0
 
@@ -15,12 +10,4 @@ for i in range(num_case):
   for j in range(int(s_len/2)):
     if s[j] is not s[s_len-(j+1)]:
       obs_k+=1
-  if obs_k < exp_k:
-    l.append(exp_k-obs_k)
-  elif exp_k < obs_k:
-    l.append(exp_k-obs_k)
-  else :
-    l.append(0)  
-
-for i in range(len(l)):
-  print(f"Case #{i}: {l[i]}\n")
+  print(f"Case #{i+1}: {abs(exp_k-obs_k)}\n")
